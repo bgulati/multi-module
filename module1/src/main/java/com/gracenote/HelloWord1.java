71a92ba3c354a58d19bf7e8ce62a98866d3eb4c9
Bhupinder Gulati <bgulati@gracenote.com>
2017-09-29 14:18:33 -0700

    boo

---
+++ module1/src/main/java/com/gracenote/HelloWord1.java
@@ -0,0 +1,10 @@
+package com.gracenote;
+
+/**
+ * Created by bgulati on 9/29/17.
+ */
+public class HelloWord1 {
+    public static void main(String[] args) {
+        System.out.println("Hello world from Module 1");
+    }
+}
